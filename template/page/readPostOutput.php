481d2b21ea23e5db8d6ed7caaa9f0093f5996f22
namePlayer <r06sollfrank@gmail.com>
2021-11-16 21:15:24 +0100

    Style up some sidebar

--- template/page/readPostOutput.php
+++ template/page/readPostOutput.php
@@ -22,7 +22,9 @@
                 <div class="card shadow-sm">
                     <div class="card-body">
                         <h4>What is this?</h4>
-                        <p>With this service you can share Text </p>
+                        <p>With this service you can share Text anonymously. Also you can <b>format</b> <u>your</u> <s>Text</s> with <i>Markdown</i></p>
+                        <hr>
+                        <p>Want some example? Visit our Demonstration file <a href="<?= $router->readAndOutputRequestedPath() ?>/read/demo">demo</a></p>
                     </div>
                 </div>
             </div>
