6503f440d3ad32c890d2e70b8228ec9e5ef1d838
namePlayer <r06sollfrank@gmail.com>
2021-11-19 13:11:31 +0100

    Some visual improvements

--- template/page/readPostOutput.php
+++ template/page/readPostOutput.php
@@ -31,14 +31,7 @@
                 <?= $markdownFromDocumentAsHtml ?>
             </div>
             <div class="col-md-4">
-                <div class="card shadow-sm">
-                    <div class="card-body">
-                        <h4>What is this?</h4>
-                        <p>With this service you can share Text anonymously. Also you can <b>format</b> <u>your</u> <s>Text</s> with <i>Markdown</i></p>
-                        <hr>
-                        <p>Want some example? Visit our Demonstration file <a href="<?= $router->readAndOutputRequestedPath() ?>/read/demo">demo</a></p>
-                    </div>
-                </div>
+                <?= $templateEngine->render('sidebar', ['router' => $router]) ?>
             </div>
         </div>
     </div>
